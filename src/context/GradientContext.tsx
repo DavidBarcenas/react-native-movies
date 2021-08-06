04c337135aabd1ebc333f773ba5be1fd4b81a614
David Barcenas <daveepro@outlook.com>
2021-08-05 20:17:13 -0500

    add gradient context provider

---
+++ src/context/GradientContext.tsx
@@ -0,0 +1,46 @@
+import React, { createContext, useState } from 'react';
+
+interface ImageColors {
+    primary: string;
+    secondary: string;
+}
+
+interface ContextProps {
+    colors: ImageColors;
+    prevColors: ImageColors;
+    setImageColors: (colors: ImageColors) => void;
+    setPrevImageColors: (colors: ImageColors) => void;
+}
+
+export const GradientContext = createContext({} as ContextProps)
+
+export const GradientProvider = ({children}: any) => {
+    const [prevColors, setPrevColors] = useState<ImageColors>({
+        primary: 'transparent',
+        secondary: 'transparent'
+    })
+
+    const [colors, setColors] = useState<ImageColors>({
+        primary: 'transparent',
+        secondary: 'transparent'
+    })
+
+    const setImageColors = (colors: ImageColors) => {
+        setColors(colors)
+    }
+
+    const setPrevImageColors = (colors: ImageColors) => {
+        setPrevColors(colors)
+    }
+
+    return (
+        <GradientContext.Provider value={{
+            colors, 
+            prevColors, 
+            setImageColors, 
+            setPrevImageColors
+        }}>
+            {children}
+        </GradientContext.Provider>
+    )
+}
