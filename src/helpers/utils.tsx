10510e0394e77138ce39031b7cce7520a36c1ddc
David Barcenas <daveepro@outlook.com>
2021-08-10 15:54:30 -0500

    refactor actor screen

--- src/helpers/utils.tsx
+++ src/helpers/utils.tsx
@@ -1,3 +1,14 @@
+export const getDate = (date: string | null | undefined) => {
+    let newDate = '';
+    
+    if(date) {
+        const split = date.split('-')
+        newDate = `${split[2]}-${split[1]}-${split[0]}`
+    }
+
+    return newDate
+}
+
 export const getTime = (time: number) => {
     const hours   = (time / 60).toFixed(0).toString()
     const minutes = (time % 60).toFixed(0).toString()
--- src/screens/ActorScreen.tsx
+++ src/screens/ActorScreen.tsx
@@ -1,7 +1,6 @@
-import { StackScreenProps } from '@react-navigation/stack';
 import React, { useState } from 'react'
-import { Text, View, ScrollView, SafeAreaView, TouchableOpacity, Image, StyleSheet, FlatList } from 'react-native';
-import Icon from 'react-native-vector-icons/Ionicons';
+import { StackScreenProps } from '@react-navigation/stack';
+import { Text, View, ScrollView, SafeAreaView, TouchableOpacity, Image, StyleSheet } from 'react-native';
 
 import { movieImage } from '../api/movieAPI';
 import { RootStackParams } from '../navigation/Navigation';
@@ -10,13 +9,14 @@ import { useActorDetail } from '../hooks/useActorDetail';
 import { Spinner } from '../components/Spinner';
 import { Dimensions } from 'react-native';
 import { HorizontalSlider } from '../components/HorizontalSlider';
+import { getDate } from '../helpers/utils';
 
 interface ActorScreenProps extends StackScreenProps<RootStackParams, 'Actor'> {}
 
 const width = Dimensions.get('window').width
 
 export const ActorScreen = ({route}: ActorScreenProps) => {
-    const {name, character, profile_path, id} = route.params
+    const {character, profile_path, id} = route.params
     const {isLoading, details, movies} = useActorDetail(id)
     const [sliceText, setSliceText] = useState(true)
 
@@ -26,21 +26,25 @@ export const ActorScreen = ({route}: ActorScreenProps) => {
                 <View style={styles.header}>
                     <Image source={{uri: movieImage + profile_path}} style={styles.avatar} />
                     <View style={{width: (width - 180)}}>
-                        <Text style={styles.name}>Personaje:</Text>
+                        <Text style={styles.subtitle}>Personaje:</Text>
                         <Text style={styles.character}>{character}</Text>
                         {
                             !isLoading && (
                                 <>
                                     {
                                         details?.deathday && (
-                                            <Text style={{color: themeColors.white}}>
-                                                <Text>Muerte: </Text>
-                                                <Text>{details?.deathday}</Text>
-                                            </Text>
+                                            <>
+                                                <Text style={styles.subtitle}>Muerte:</Text>
+                                                <Text style={{color: themeColors.white}}>
+                                                    {getDate(details?.deathday)}
+                                                </Text>
+                                            </>
                                         )
                                     }
-                                    <Text style={{color: '#ccc'}}>Nacimiento: </Text>
-                                    <Text style={{color: '#fff'}}>{details?.birthday} (51 años), {details?.place_of_birth}</Text>
+                                    <Text style={styles.subtitle}>Nacimiento:</Text>
+                                    <Text style={{color: themeColors.white}}>
+                                        {getDate(details?.birthday)}, {details?.place_of_birth}
+                                    </Text>
                                 </>
                             )
                         }
@@ -49,24 +53,20 @@ export const ActorScreen = ({route}: ActorScreenProps) => {
                 {
                     isLoading ? <Spinner /> : (
                         <>                                
-                            <View style={{
-                                paddingHorizontal: 20, 
-                            }}>
+                            <View style={{paddingHorizontal: 20}}>
                                 <Text style={styles.title}>Biografía</Text>
                                 <Text style={{color: themeColors.white}}>{(
                                     details?.biography && sliceText) ? 
                                     details?.biography.slice(0, 400) + '...': 
                                     details?.biography}</Text>
                             </View>
+
                             <TouchableOpacity onPress={() => setSliceText(!sliceText)}>
-                            <Text style={{color: themeColors.white, textAlign: 'right', paddingRight: 20, marginBottom: 15}}>Leer más</Text>
+                                <Text style={styles.textMore}>Leer {sliceText ? 'mas' : 'menos'}</Text>
                             </TouchableOpacity>
-                            <View style={{paddingHorizontal: 10, marginBottom: 15}}>
-                                <Text style={{
-                                    ...styles.title, 
-                                    marginLeft: 10, 
-                                    marginBottom: 15
-                                }}>Películas populares</Text>
+
+                            <View style={{paddingHorizontal: 10}}>
+                                <Text style={{...styles.title, marginLeft: 10}}>Películas populares</Text>
                                 <HorizontalSlider movies={movies} />
                             </View>
                         </>
@@ -90,22 +90,28 @@ const styles = StyleSheet.create({
         width: size,
         height: size,
         borderRadius: 100,
-        marginRight: 20
+        marginRight: 18
     },
-    name: {
+    subtitle: {
         fontSize: 14,
         color: '#ccc',
     },
     character: {
         color: themeColors.white,
         fontWeight: '700',
-        fontSize: 18,
+        fontSize: 16,
         marginBottom: 15
     },
     title: {
-        color: themeColors.white, 
-        fontSize: 18, 
-        fontWeight: 'bold', 
-        marginBottom: 10
+        color: themeColors.white,
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        marginBottom: 15
+    },
+    textMore: {
+        color: themeColors.green, 
+        textAlign: 'right', 
+        paddingRight: 20, 
+        marginBottom: 15
     }
 })
